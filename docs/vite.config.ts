0faee0ea0de9d6170df94684397a4698772520f2
Nico <nllortfer@gmail.com>
2026-04-24 10:50:01 +0200

    Added new workflow to make the website visible

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/BiAsCo/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
